1938452929926df727621354e93e832de2e7a01d
zainalsalamun <zainalrtf@gmail.com>
2025-11-08 23:18:48 +0700

    add :  notFoundPage

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFoundPage = () => (
+  <div style={{ padding: "50px", textAlign: "center" }}>
+    <h1>404 - Halaman Tidak Ditemukan</h1>
+    <p>Periksa kembali URL yang Anda tuju.</p>
+  </div>
+);
+
+export default NotFoundPage;
